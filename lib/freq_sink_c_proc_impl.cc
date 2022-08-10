e89c311dd5bfee7cd91ebe2857430a3f2c815ecc
Cyrille Morin <barthy42@laposte.net>
2022-08-10 16:56:29 +0200

    Remove boost

--- lib/freq_sink_c_proc_impl.cc
+++ lib/freq_sink_c_proc_impl.cc
@@ -69,8 +69,7 @@ namespace gr {
       d_tmpbuf = std::vector<float>(d_tmpbuflen+1, 0);
 
       message_port_register_in(pmt::mp("freq"));
-      set_msg_handler(pmt::mp("freq"),
-                      boost::bind(&freq_sink_c_proc_impl::handle_set_freq, this, _1));
+      set_msg_handler(pmt::mp("freq"), [this](pmt::pmt_t msg) { this->handle_set_freq(msg); });
 
       buildwindow();
 
